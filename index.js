15ac6df9f39d69847cada2391b35acd46d6b9410
Khiem Vinh Tran <vinhkhiemt135@gmail.com>
2020-03-22 03:25:59 +0700

    z

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-﻿ $(function(){
- 
-})  
- 
+﻿$(window).scroll(function(){
+    var bottomheader = $('.bottomheader'),
+        scroll = $(window).scrollTop();
+  
+    if (scroll >= 100) bottomheader.addClass('fixed');
+    else bottomheader.removeClass('fixed');
+  });
